2c1d0ed78c54e82b0958e9395f96bf7ae5fa2076
albertpcoh <35908691+albertpcoh@users.noreply.github.com>
2023-03-30 18:41:44 -0400

    XD

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,22 @@ import * as React from 'react';
 import AttributeGrid from '../components/AttributeGrid';
 import Container from '../components/Container';
 import Hero from '../components/Hero';
-import BlogPreviewGrid from '../components/BlogPreviewGrid';
+//import BlogPreviewGrid from '../components/BlogPreviewGrid';
 import Highlight from '../components/Highlight';
 import Layout from '../components/Layout/Layout';
-import ProductCollectionGrid from '../components/ProductCollectionGrid';
-import ProductCardGrid from '../components/ProductCardGrid';
+//import ProductCollectionGrid from '../components/ProductCollectionGrid';
+//import ProductCardGrid from '../components/ProductCardGrid';
 import Quote from '../components/Quote';
 import Title from '../components/Title';
 
 import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
 
 import * as styles from './index.module.css';
-import { Link, navigate } from 'gatsby';
+import { navigate } from 'gatsby';
 import Highlight2 from '../components/Highlight/highlight2';
 
 const IndexPage = () => {
-  const newArrivals = generateMockProductData(3, 'shirt');
-  const blogData = generateMockBlogData(3);
-
+  
   const goToShop = () => {
     navigate('/shop');
   };
